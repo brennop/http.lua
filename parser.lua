2e0c5ad9930b4aac721d50b1246cba42dadfb1f8
brennop <brennopcordeiro@gmail.com>
2024-03-25 14:18:37 -0300

    simplify parser

--- parser.lua
+++ parser.lua
@@ -1,49 +1,32 @@
 function parser(data)
-  local pattern, version, rest
-  local body = ""
+  local pattern, version, rest, body
   local headers = { }
 
   -- parse request line
   while pattern == nil do
-    pattern, version, rest = 
-        data:match("(%u+%s[%p%w]+)%s(HTTP/1.1)\r\n(.*)")
+    pattern, version, rest = data:match("(%u+%s[%p%w]+)%s(HTTP/1.1)\r\n(.*)")
 
-    if pattern then
-      data = rest
-    else
-      data = data .. coroutine.yield()
-    end
+    data = rest or (data .. coroutine.yield())
   end
 
   -- parse headers
-  while true do
-    local key, value, rest = 
-        data:match("([%w%-]+):%s([%p%w]+)\r\n(.*)")
+  while not data:match("^\r\n(.*)") do
+    local key, value, rest = data:match("([%w%-]+):%s([%p%w]+)\r\n(.*)")
 
-    if key then
-      headers[key] = value
-      data = rest
-    else
-      local match = data:match("\r\n(.*)")
-
-      if match then
-        data = match
-        break
-      end
-
-      data = data .. coroutine.yield()
-    end
+    if key then headers[key] = value end
+    
+    data = rest or (data .. coroutine.yield())
   end
 
   -- parse body
   if headers["Content-Length"] then
     local length = tonumber(headers["Content-Length"])
 
-    while #data < length do
+    while #data - 2 < length do
       data = data .. coroutine.yield()
     end
 
-    body = data:sub(1, length)
+    body = data:sub(3, length + 2)
   end
 
   return { pattern = pattern, version = version, headers = headers, body = body }
